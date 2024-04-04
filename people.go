5cd48a4f164e8c60c2953182dc54a35d925eb609
edwinjavier28 <erico.andes@gmail.com>
2024-04-04 15:58:57 -0500

    new file people.go + funcion suma

---
+++ people.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Definimos dos variables para sumar
+	a := 5
+	b := 3
+
+	// Realizamos la suma
+	resultado := a + b
+
+	// Mostramos el resultado
+	fmt.Println("El resultado de la suma es:", resultado)
+}
